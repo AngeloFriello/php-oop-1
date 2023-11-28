b111647fc272d42b01bdbaef60938511d64f56c8
angelo friello <angelofriello01@gmail.com>
2023-11-28 14:58:55 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+
+class Production{
+    public $title;
+    public $leanguage;
+    public $rating;
+    
+    function __construct(String $_title, $_leanguage, $_rating) {
+
+        $this->var = $var;
+    }
+}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+</body>
+</html>
+
